dbe78ab4753ca78810ad8789d6d1b50252e7dfdf
lowfront <fronterior@hotmail.com>
2022-05-06 00:50:11 +0900

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return <footer className="info">
     <p>Double-click to edit a todo</p>
-    <p>Created by <a href="https://github.com/lowfront">Lowfront</a></p>
+    <p>Created by <a href="https://github.com/lowfront/next-auth-firestore-adapter-example">Lowfront</a></p>
     <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
   </footer>
 }
