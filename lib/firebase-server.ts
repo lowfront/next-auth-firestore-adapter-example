3e863afb8567ddc85ae3789868a7f3f6977fa9f4
lowfront <fronterior@hotmail.com>
2022-05-06 00:19:39 +0900

    update

--- lib/firebase-server.ts
+++ lib/firebase-server.ts
@@ -12,7 +12,7 @@ if (!admin.apps.length) {
       type: process.env.FIREBASE_ADMIN_CONFIG_type,
       project_id: process.env.FIREBASE_ADMIN_CONFIG_project_id,
       private_key_id: process.env.FIREBASE_ADMIN_CONFIG_private_key_id,
-      private_key: process.env.FIREBASE_ADMIN_CONFIG_private_key,
+      private_key: process.env.FIREBASE_ADMIN_CONFIG_private_key?.replace(/\\n/gm, '\n'), // https://github.com/gladly-team/next-firebase-auth/discussions/95#discussioncomment-473663
       client_email: process.env.FIREBASE_ADMIN_CONFIG_client_email,
       client_id: process.env.FIREBASE_ADMIN_CONFIG_client_id,
       auth_uri: process.env.FIREBASE_ADMIN_CONFIG_auth_uri,
